1651123743a829347443807a1bb0e5b93350f45b
Sushruth <ssushruth2003@gmail.com>
2023-10-01 13:56:17 -0700

    contribution!

---
+++ C++/Even or Odd.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+using namespace std;
+int main(){
+    cout <<"Enter number :";
+    cin >> number;
+    if(number % 2 == 0) 
+        cout << "Even !";
+    else 
+        cout << "Odd !";
+    return 0;
+}
