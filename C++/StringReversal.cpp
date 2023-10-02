dcfa993cbabafa2588db35e8404582239d5f78bd
abhinavmsn <59202801+FazeZxc@users.noreply.github.com>
2023-10-02 16:39:28 +0530

    i abhinavmsn made a contribution

--- C++/StringReversal.cpp
+++ C++/StringReversal.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
 
+
+
+void rev(string &arr,int s,int e)
+{    
+    if(s>e){
+        return;
+    }  
+    swap(arr[s],arr[e]);
+    
+    s++;
+    e--;
+    rev(arr,s,e);
+}
+int main(){
+   string arr="abcde";
+   rev(arr,0,arr.length()-1);
+   cout<< arr;
+}
