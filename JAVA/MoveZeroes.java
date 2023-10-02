05c15782acca65ee3d9cfd3be3eac1ca756a9fbe
DinithKumudika <dinithwalpitagama@gmail.com>
2023-10-03 02:36:51 +0530

    I DinithKumudika  made a contribution!

---
+++ JAVA/MoveZeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+     public void moveZeroes(int[] nums) {
+          int i = 0;
+          for (int num:nums) {
+               if(num!=0){
+                    nums[i] = num;
+                    i++;
+               }
+               while(i < nums.length) {
+                    nums[i] = 0;
+                    i++;
+               }
+          }
+     }
+}
